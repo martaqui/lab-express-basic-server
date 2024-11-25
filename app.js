94d5e496a977f355802182d340d663e3feb0f04a
Marta Quiroga Cuervo <martaquirogacuervo@MacBook-Pro-de-Marta.local>
2024-11-25 16:19:59 +0100

    lab express basic done

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-// IMPORT PACKAGES
-// Here you should import the required packages for your Express app: `express` and `morgan`
+const express = require('express')
+const logger = require('morgan')
 
 
+const app = express()
 
-// CREATE EXPRESS APP
-// Here you should create your Express app:
+app.use(logger('dev'))
+app.use(express.json())
 
+const projects = require("./data/projects.json")
+const articles = require("./data/articles.json")
 
+app.get("/api/projects", (req, res) => {
+    console.log('FUNCIONA')
+    res.json(projects)
+})
 
-// MIDDLEWARE
-// Here you should set up the required middleware:
-// - `express.static()` to serve static files from the `public` folder
-// - `express.json()` to parse incoming requests with JSON payloads
-// - `morgan` logger to log all incoming requests
+app.get("/api/articles", (req, res) => {
+    console.log('aaaaaaa')
+    res.json(articles)
+})
 
+app.get('*', (req, res) => {
+    res.status(404).send('notfound')
+})
 
-
-// ROUTES
-// Start defining your routes here:
-
-
-
-// START THE SERVER
-// Make your Express server listen on port 5005:
+app.listen(5005, () => console.log('server running en este puerto'))
